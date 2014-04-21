c894ce55964a95dc9940749769019768032b6926
Zhihao Jia <zhihao@corn14.stanford.edu>
2014-04-20 21:00:36 -0700

    finish part 1 of project 1

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -89,6 +89,8 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
+    struct list_elem sleepelem;
+    int64_t wake_up_time;
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
