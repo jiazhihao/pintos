069f799da57287c1f1906edc6d728f257b586422
Tai Guo <taig@corn10.stanford.edu>
2014-05-03 22:33:29 -0700

    passing parameter

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -2,10 +2,15 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
-
+#define FILE_NAME_LEN 15
+#define LONG_SIZE 4
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
+
+bool get_file_name (char *cmd_line, char *file_name);
+bool argv_passer (char *argv, void **esp);
+bool calculate_len (char *argv, int *argc, int *len);
 
 #endif /* userprog/process.h */
